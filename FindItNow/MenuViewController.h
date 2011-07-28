0a507163e8773f655a49992e339d74e52ca75a83
Chanel Huang <chanelhuang@chanel-huangde-macbook-pro.local>
2011-07-27 22:26:20 -0700

    Menu View Controller Files

---
+++ FindItNow/MenuViewController.h
@@ -0,0 +1,23 @@
+//
+//  MenuViewController.h
+//  FindItNow
+//
+//  Created by Chanel Huang on 2011/7/26.
+//  Copyright 2011年 University of Washington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class MapView;
+
+@interface MenuViewController : UIViewController {
+    
+}
+@property (nonatomic, retain) IBOutlet MapView *mapView;
+@property (nonatomic, retain) IBOutlet UILabel *text;
+@property (nonatomic, retain) IBOutlet UIScrollView *btnGrid;
+
+- (IBAction) getCategory ;
+- (IBAction)map ;
+- (void) initBtnGrid;
+
+@end
---
+++ FindItNow/MenuViewController.m
@@ -0,0 +1,96 @@
+//
+//  MenuViewController.m
+//  FindItNow
+//
+//  Created by Chanel Huang on 2011/7/26.
+//  Copyright 2011年 University of Washington. All rights reserved.
+//
+
+#import "MenuViewController.h"
+#import "MapView.h"
+
+@implementation MenuViewController
+@synthesize mapView;
+@synthesize btnGrid;
+@synthesize text;
+
+
+- (void) initBtnGrid
+{
+    NSURL *URL=[[NSURL alloc] initWithString:@"http://www.fincdn.org/getCategories.php"];
+    NSString *results = [[NSString alloc] initWithContentsOfURL :URL];
+    
+    NSArray *categories = [results componentsSeparatedByString:@","];
+    NSMutableArray *buttons = [[NSMutableArray alloc] initWithCapacity:[categories count]];
+    int btnSize = ( CGRectGetHeight(btnGrid.frame) - 50 )/([categories count]/2);
+    //int btnWidth = CGRectGetWidth(btnGrid.frame) / ([categories count
+    for (int i=0; i < [categories count]; i = i+1) {
+        UIButton *butt = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        
+        butt.frame = CGRectMake(25+((i%2)*(btnSize+50)), 25+((i/2)*btnSize), btnSize, btnSize);
+        
+        [butt setTitle:[categories objectAtIndex:i] forState:UIControlStateNormal];
+        [btnGrid addSubview:butt];
+        [buttons addObject:butt];
+    }
+}
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        //[self initBtnGrid];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self initBtnGrid];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+- (IBAction)map {
+    //MapView *temp = [ [MapView alloc] init];
+    [self.tabBarController.view addSubview:mapView];
+}
+
+- (IBAction) getCategory {
+    NSURL *URL=[[NSURL alloc] initWithString:@"http://www.fincdn.org/getCategories.php"];
+    NSString *results = [[NSString alloc] initWithContentsOfURL :URL];
+    [text setText:results ];
+    [text setCenter:CGPointMake(100, 150)];
+}
+
+@end
