44f0dba52f4297588788fe4cf37f8157069e5ed4
Eric Hare <Raftysworld@gmail.com>
2011-08-17 10:04:57 -0700

    Overlay supporttttt

---
+++ ItemAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  ItemAnnotation.h
+//  FindItNow
+//
+//  Created by Eric Hare on 8/16/11.
+//  Copyright 2011 University of Washington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItemAnnotation : NSObject
+
+@end
---
+++ ItemAnnotation.m
@@ -0,0 +1,23 @@
+//
+//  ItemAnnotation.m
+//  FindItNow
+//
+//  Created by Eric Hare on 8/16/11.
+//  Copyright 2011 University of Washington. All rights reserved.
+//
+
+#import "ItemAnnotation.h"
+
+@implementation ItemAnnotation
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
